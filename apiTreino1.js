fe9974c2ec4540ea9e917f05ec77c1f66d1549da
voltareli <nivoltareli@gmail.com>
2024-08-02 18:12:12 -0300

    subtracao feita!!!!

--- apiTreino1.js
+++ apiTreino1.js
@@ -12,4 +12,25 @@ servidor.get('/calculadora/soma/:n1/:n2',(req,resp)=>{
     })
 })
 
+
+
+servidor.get('/calculadora/subtracao/:n1/:n2',(req,resp)=>{
+    let n1 = Number(req.params.n1) ;
+    let n2 = Number(req.params.n2) ;
+    let subtracao = n1-n2;
+    
+    resp.send({
+        subtracao : subtracao
+    })
+})
+
+
+
+
+
+
+
+
+
+
 servidor.listen(5050, () => console.log(`API SUBINDO NA PORTA 5050`));
